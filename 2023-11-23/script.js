5993a7e9a8dc90374e41ba6560a7fd29eff1efc5
Lv4e <144341809+Lv4e@users.noreply.github.com>
2023-11-30 14:14:07 +0100

    Add files via upload

--- 2023-11-23/script.js
+++ 2023-11-23/script.js
@@ -1,2 +1,38 @@
-  const numberButtons = document.querySelectorAll('[data-number]')
+ class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
+clear(){
+  	this.currentOperand = ""
+    this.previousOperand = ""
+    this.operation = undefined
+}
+delete() {
+
+}
+
+appendNumber() {
+
+}
+
+chooseOperation() {
+
+}
+compute() {
+
+}
+
+updateDisplay() {
+
+}
+ }
+ 
+ const numberButtons = document.querySelectorAll('[data-number]')
   const operationButtons = document.querySelectorAll('[dataoperation]')
+  const equalsButton = document.querySelectorAll('data-equals')
+  const deleteButton = document.querySelectorAll('data-delete')
+  const allClearButton = document.querySelectorAll('data-all-clear')
+  const previousOperandTextElement = document.querySelectorAll('data-previous-operand')
+  const currentOperandTextElement = document.querySelectorAll('data-current-operand')
