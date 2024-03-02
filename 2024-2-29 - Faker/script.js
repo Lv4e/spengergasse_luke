5878fe49acf858c244e042f82e1fc4000b462846
Luke <144341809+Lv4e@users.noreply.github.com>
2024-03-02 14:20:17 +0100

    Faker

--- 2024-2-29 - Faker/script.js
+++ 2024-2-29 - Faker/script.js
@@ -3,5 +3,6 @@ const faker = require('faker');
 const email = faker.internet.email();
 const firstName = faker.name.firstName();
 const lastName = faker.name.lastName();
+const animal = faker.animal.animal();
 
 console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email}`);
