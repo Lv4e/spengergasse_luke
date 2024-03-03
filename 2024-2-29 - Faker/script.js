52cd0abd674b56cfabc375d2fc5e83f8a9ddff22
Luke <144341809+Lv4e@users.noreply.github.com>
2024-03-03 22:40:39 +0100

    Faker 2.0

--- 2024-2-29 - Faker/script.js
+++ 2024-2-29 - Faker/script.js
@@ -4,5 +4,56 @@ const email = faker.internet.email();
 const firstName = faker.name.firstName();
 const lastName = faker.name.lastName();
 const animal = faker.animal.animal();
+const city = faker.address.city();
 
-console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email}`);
+console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email} lives in ${city} and has a ${animal} as a pet.`);
+
+const faker = require('faker');
+
+const email = faker.internet.email();
+const firstName = faker.name.firstName();
+const lastName = faker.name.lastName();
+const animal = faker.animal.animal();
+const city = faker.address.city();
+
+console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email} lives in ${city} and has a ${animal} as a pet.`);
+
+const faker = require('faker');
+
+const email = faker.internet.email();
+const firstName = faker.name.firstName();
+const lastName = faker.name.lastName();
+const animal = faker.animal.animal();
+const city = faker.address.city();
+
+console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email} lives in ${city} and has a ${animal} as a pet.`);
+
+const faker = require('faker');
+
+const email = faker.internet.email();
+const firstName = faker.name.firstName();
+const lastName = faker.name.lastName();
+const animal = faker.animal.animal();
+const city = faker.address.city();
+
+console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email} lives in ${city} and has a ${animal} as a pet.`);
+
+const faker = require('faker');
+
+const email = faker.internet.email();
+const firstName = faker.name.firstName();
+const lastName = faker.name.lastName();
+const animal = faker.animal.animal();
+const city = faker.address.city();
+
+console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email} lives in ${city} and has a ${animal} as a pet.`);
+
+const faker = require('faker');
+
+const email = faker.internet.email();
+const firstName = faker.name.firstName();
+const lastName = faker.name.lastName();
+const animal = faker.animal.animal();
+const city = faker.address.city();
+
+console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email} lives in ${city} and has a ${animal} as a pet.`);
