98db1e669fc70c722d5af9184f41f5c1e629c008
Luke <144341809+Lv4e@users.noreply.github.com>
2024-03-06 11:40:11 +0100

    faker

--- 2024-2-29 - Faker/script.js
+++ 2024-2-29 - Faker/script.js
@@ -4,13 +4,13 @@ function generateData() {
     const email = faker.internet.email();
     const firstName = faker.name.firstName();
     const lastName = faker.name.lastName();
-    const sextype = faker.name.sextype();
+    const jobarea = faker.name.jobArea();
     const city = faker.address.city();
 
-    console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email} lives in ${city} and is a ${sextype}`);
+    console.log(`Firstname: ${firstName}, last name: ${lastName}, Email: ${email} lives in ${city} and works in ${jobarea}.`);
 }
 
 
-for(let i = 0; i < 9; i++) {
+for(let i = 0; i < 10; i++) {
     generateData();
 }
