86a2085b6f2ad2491e60ba6deec00a71177abf61
Lv4e <klu230103@spengergasse.at>
2024-05-20 22:16:01 +0200

    Uebung fuer test

---
+++ 2024-4-25 --rnd number/script.js
@@ -0,0 +1,31 @@
+const rl = require('readline').createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+let count = 0;
+let i = Math.random(1, 100) * 100;
+
+function nummer () {
+    count++;
+    rl.question('Sag zahl du kek \n', (useranwser) => {
+       
+if (useranwser == i.toFixed(0)) {
+    console.log('richtig')
+    console.log(count)
+    rl.close();
+} 
+else if (useranwser > i){
+    console.log('too higher')
+    nummer();
+}
+else if (useranwser < i){
+    console.log('too lower')
+    nummer();
+}
+    });
+
+    
+}
+console.log(i.toFixed(0))
+
+nummer();
